36279eb1f95a0c888c69e00c56019e43fb3eaa51
MoonTeamDev <makan.j130130@gmail.com>
2016-12-30 02:23:26 +0330

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -91,9 +91,9 @@ function tdcli_update_callback(data)
         tdcli.sendMessage(chat_id, msg.id_, 1, '<b>SuperGroup ID : </b><code>'..string.sub(chat_id, 5,14)..'</code>\n<b>User ID : </b><code>'..user_id..'</code>', 1, 'html')
       end
 		
-	if input:match('slm') and editMessageText then		
-text = '<b>dont edit</b>'
-tdcli.sendMessage(msg.chat_id_, 0, 0, 1, nil, text, 1, 'html')		
+	if input:match("^slm") and editMessageText then		
+tdcli.sendMessage(chat_id, reply_id, 1, 'dont edit :|', 'html')
+		
 end					
 if input:match('^/add$') and is_sudo(msg) then
 redis:set('add_rem'..msg.chat_id_,true)
